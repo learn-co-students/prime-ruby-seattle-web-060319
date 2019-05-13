03535e59175a4a6415b5ce3b8c5e4d7846127366
Michael Maggs <michaelmaggs@gmail.com>
2019-05-13 00:24:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2 
+    return true
+  end
+  (2...num).each do |number|
+    if num != 2 && num % number == 0
+      return false
+    end
+  end
+  return true
+end
+    
+
+
 # Add  code here!
