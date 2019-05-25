00cff1b7f5aa403661600add1cd800f58c6d5e30
github username <github email address>
2019-05-24 23:59:56 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+	if num < 4 && num > 1
+		return true
+	elsif num <= 1
+		return false
+	else
+		arr = Array(2...num)
+		arr.each do |i|
+			if i ** (num-1) % num == 1
+				return true
+			else
+				return false
+			end
+		end
+	end
+end
